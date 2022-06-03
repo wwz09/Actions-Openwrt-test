3e4f566e9683b1e7935773b2f8a9ccd2c928a1a7
wwz09 <47264059+wwz09@users.noreply.github.com>
2022-06-03 21:49:19 +0800

    Update diy-part8.sh

--- diy-part8.sh
+++ diy-part8.sh
@@ -12,7 +12,7 @@
 
 # '修改默认IP'
 
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
 
 
 # '修改默认主机名'
