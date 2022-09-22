fd2de3f5c18107708af45934c9dfe9f4fe49d91e
wwz09 <47264059+wwz09@users.noreply.github.com>
2022-09-23 07:30:24 +0800

    Update CM520.sh

--- CM520.sh
+++ CM520.sh
@@ -14,7 +14,7 @@
 # sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/ipq40xx/Makefile         # ipq40xx，内核：5.4，内核为5.10
 # 修改默认IP
 
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 #修改默认主机名
 sed -i 's/OpenWrt/CM520/g' package/base-files/files/bin/config_generate
